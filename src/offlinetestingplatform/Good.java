c3aed4401ced1f242adfc4a67059221161410186
Re42 <33805964+Re42@users.noreply.github.com>
2018-11-10 17:57:45 +0530

    change name for good class to start with lower case

--- src/offlinetestingplatform/Good.java
+++ src/offlinetestingplatform/Good.java
@@ -2,9 +2,9 @@ package offlinetestingplatform;
 
 import java.util.ArrayList;
 import java.util.Dictionary;
-public class Good extends question {
+public class good extends question {
 
-  public Good(String _content, ArrayList<String> _options, int _correct) {
+  public good(String _content, ArrayList<String> _options, int _correct) {
 		super(_content, _options, _correct);
 		// TODO Auto-generated constructor stub
 	}
